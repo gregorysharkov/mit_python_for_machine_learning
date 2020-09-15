5ce28a399e181076e63a22ddf8bc5e87ec395fa3
Grigory Sharkov <gregorysharkov@gmail.com>
2020-09-16 00:24:01 +0200

    day2

--- problem_1.py
+++ problem_1.py
@@ -7,34 +7,37 @@ Created on Mon Sep 14 00:31:46 2020
 
 import numpy as np
 
-x = np.array([[-1,-1,1],[1,0,1],[-1,1.5,1]])
+x = np.array([[-1,-1],[1,0],[-1,1.5]])
 y = np.array([1,-1,1])
 
+alt_x = np.array([[1,0],[-1,1.5],[-1,-1]])
+alt_y = np.array([1,-1,1])
+
 def perception(x,y):
-    theta = np.array([0.,0.,0.])
-    for t in range(len(x)):
-        print(f"Step {t+1}:")
-        print(f"\tOriginal theta: {theta}")
-        check = y[t]*theta*x[t].sum()
-#        print(f"\ty[t]*theta*x[t]: {check}")
-        print(f"\tcheck: {check.sum()}")
-        if check.sum() <= 0:
-            theta += y[t]*x[t]
-            print(f"\tUpdating theta, new theta: {theta}")
-    
-    print(f"Final theta: {theta}")
-    return theta
+    theta = np.array([0.,0.])
+    all_thetas = []
+    theta_changed = True
+    i=0
+    while theta_changed:
+        theta_changed=False
+        print(f"Iteration {i+1}")
+        for t in range(len(x)):
+            check = y[t]*theta*x[t]
+            print(y[t],theta,x[t],check.sum())
+            if check.sum() <= 0:
+                theta += y[t]*x[t]
+                all_thetas.append(list(theta.copy()))
+                theta_changed = True
+        i+=1
+    return theta, all_thetas
 
 def predict(x,y):
-    theta = perception(x,y)
+    theta, all_thetas = perception(x,y)
     print("Predicting...")
-    print(f"\ttheta: {theta}")
+    print(f"\ttheta: {theta}\n\tall_thetas: {all_thetas}")
     for i in range(len(x)):
         prediction = x[i]*theta
-        print(f"\tx_{i+1}: {prediction.sum()}")
-
-alt_x = np.array([[1,0,1],[-1,1.5,1],[-1,-1,1]])
-alt_y = np.array([1,-1,1])
+        print(f"\tx_{i+1}: {prediction.sum()>=0}")
 
 predict(x,y)
 predict(alt_x,alt_y)
---
+++ problem_1_1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 22:30:27 2020
+
+@author: Grigory
+"""
+
+import numpy as np
+
+x = [[-1,-1],[1,0],[-1,1.5]]
+y = [1,0,1]
+
+alt_x = [[1,0],[-1,1.5],[-1,-1]]
+alt_y = [0,1,1]
+
+
+def perceptron(features,labels, num_iter):
+    # set weights to zero
+    w = np.zeros(shape=(1,len(features)))
+    weights = []
+    misclassified_ = [] 
+  
+    for epoch in range(num_iter):
+        misclassified = 0
+        for x, label in zip(features, labels):
+            #print(x)
+            #x = np.array(x)
+            x = np.insert(x,0,1)
+            y = np.dot(w, x.transpose())
+            target = 1.0 if (y > 0) else 0.0
+            
+            delta = (label - target)
+
+        
+            if(delta): # misclassified
+                misclassified += 1
+                w += (delta * x)
+                weights.append(list(w.copy()))
+            #print(f"y: {y}, w: {w}, x: {x}, delta: {delta}, miss: {misclassified}")  
+             
+        misclassified_.append(misclassified)
+    return (w, misclassified_, weights)
+             
+num_iter = 5
+w, misclassified_, weights = perceptron(x, y, num_iter)
+print(misclassified_, weights)
+w, misclassified_, weights = perceptron(alt_x, alt_y, num_iter)
+print(misclassified_,weights)
+#print (w,misclassified_)
